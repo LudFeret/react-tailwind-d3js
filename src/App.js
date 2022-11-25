b57db1862ff52733626fc679a85bd16a57ece1ca
Ludovic Feret <ludovic.feret@appines.fr>
2022-11-26 00:08:03 +0100

    Adding TS

--- src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-   <h1 className='text-blue font-size text-6xl text-center mt-10'>Hello World</h1>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+  const title: string = 'Hello World !'
+  return (
+   <h1 className='text-blue font-size text-6xl text-center mt-10'>{title}</h1>
+  );
+}
+
+export default App;
